99753c51d98544ed1307fbd45a0424df3a7099d5
3arglop <estefania.arguetalopez13@gmail.com>
2022-10-02 17:55:12 -0500

    Added the fetchGLITCH function into JS file

--- movies.js
+++ movies.js
@@ -1,2 +1,15 @@
-"use strict"
+"use strict";
+
+const glitchURL = 'https://aluminum-coral-comic.glitch.me/movies';
+
+const fetchGLITCH = async () => {
+    try {
+        //DEFINE THE RESPONSE TO GET THE DATA
+        const res = await axios.get(glitchURL);
+        console.log(res.data);
+        return res.data;
+    } catch(e) { //e is not event in this case... it is an ERROR event
+        console.log("Fetch Glitch Error!", e);
+    }
+}
 
